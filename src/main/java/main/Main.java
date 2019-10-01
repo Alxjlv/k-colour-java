b809992d38f2c017925f5c896f86f9a8f8b201a3
Alxjlv <alxjlv@gmail.com>
2019-10-01 21:38:54 +1300

    Setup main fxml loading

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,12 @@
 package main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -11,6 +16,17 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        try{
+            primaryStage.setTitle("K-colour Image converter");
+            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/LandingPage.fxml"));
+            Parent layout = loader.load();
+            Scene scene = new Scene(layout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
